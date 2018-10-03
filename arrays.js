ccc289f50264cc526455cf578319314b2e2b6600
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-03 10:46:24 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kit kat',
+  'skittles'
+]
+function addElementToBeginningOfArray(chocolateBars, toblerone){
+  return [toblerone, ...chocolateBars];
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, toblerone){
+  chocolateBars.unshift(toblerone);
+  return chocolateBars;
+}
+function addElementToEndOfArray(chocolateBars, toblerone){
+  return [...chocolateBars, toblerone];
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, toblerone){
+  chocolateBars.push(toblerone);
+  return chocolateBars;
+}
+function accessElementInArray (array, index){
+ return array [index]; 
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
